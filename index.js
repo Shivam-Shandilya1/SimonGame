5ce3c67a3a5a229d56e3e38727b4df321d231e6c
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-15 11:44:12 +0530

    write some js code

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-$("h1").css("color","red");
- $(document).keydown(function(event){
-     var as = event.key;
-    $("h1").text(as);
-});
+
+var randum = Math.random()*4 ;
+var randomnum = Math.ceil(randum);
+console.log(randomnum);
+function playgreen()
+{
+    var audio =new Audio('sounds/green.mp3');
+    audio.play();
+}
+function playred()
+{
+    var audio =new Audio('sounds/red.mp3');
+    audio.play();
+}
+function playyellow()
+{
+    var audio =new Audio('sounds/yellow.mp3');
+    audio.play();
+}
+function playblue()
+{
+    var audio =new Audio('sounds/blue.mp3');
+    audio.play();
+}
+
+
+$(".green").click(playgreen);
+$(".red").click(playred);
+$(".yellow").click(playyellow);
+$(".blue").click(playblue);
