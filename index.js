50a42cf482adf0873b73ba503705f8f949013fdb
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-23 20:18:07 +0530

    Commit

--- index.js
+++ index.js
@@ -18,27 +18,41 @@ function playblue()
     var audio =new Audio('sounds/blue.mp3');
     audio.play();
 }
+function animation()
+{
+   $(".btn").click(function()
+   {
+      this.addClass("pressed");
+    this.setTimeout(function(){
+        this.removeClass("pressed"),4000
+    }) 
+   }) 
+}
 function audiostop()
 {
 $("#my_audio").trigger('pause');
 }
 $(".green").click(playgreen);
+$(".green").click(animation);
 $(".red").click(playred);
+$(".red").click(animation);
 $(".yellow").click(playyellow);
+$(".yellow").click(animation);
 $(".blue").click(playblue);
+$(".blue").click(animation);
 var randum = Math.random()*4 ;
 var randomnum = Math.ceil(randum);
 console.log(randomnum);
 
 if(randomnum == 1)
 {
-    $("#my_audio").attr("src","sounds/green.mp3");
-
+    $("#my_audio").attr("href","sounds/green.mp3");
+    
 }
-
 if(randomnum == 2)
 {
     $("#my_audio").attr("src","sounds/red.mp3");
+   
 }
 
 if(randomnum == 3)
@@ -48,11 +62,10 @@ if(randomnum == 3)
 
 if(randomnum == 4)
 {
-    $("#my_audio"). attr("src","sounds/blue.mp3");
+    $("#my_audio"). attr("src","sounds/blue.mp3");   
 }
 $(document).ready(function()
 {
 $("#my_audio").trigger('play');
-window.setTimeout(audiostop,300);
-
+window.setTimeout(audiostop,350);
 });
